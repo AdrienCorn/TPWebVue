59c12898b20ed12112e24a61e39d0ec260e17750
AdrienCorn <adrien.corn@wanadoo.fr>
2021-04-28 21:24:46 +0200

    suppr stop button

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="container">
     <div id="result">Currently Saying: </div><br>
-    <button id="commencer" v-on:click="commencer"><img src="./Images/mic_icon.jpg" height="51" width="51"/></button>
+    <button id="commencer" v-on:click="record"><img src="./Images/mic_icon.jpg" height="51" width="51"/></button>
     <img id="bubble" src="./Images/bubble.jpg" height="28" width="82"/>
   </div>
-  <button id="arreter" v-on:click="arreter">arreter</button>
 </template>
 
 <script>
@@ -12,6 +11,7 @@ export default {
   name: 'App',
   data() {
     return {
+      isRecord : false,
       recognition : new window.webkitSpeechRecognition(),
       result_transcript : '',
       keyWord : ["vibre","tremble","vibration"],
@@ -22,30 +22,33 @@ export default {
       navigator.vibrate(200);
       console.log("vibre");
     },
-    commencer() {
-      let vm = this;
-      this.recognition.continuous = true;
-      this.recognition.interimResults = true;
-      this.recognition.start();
+    record() {
+      if(!this.isRecord) {
+        this.isRecord = !this.isRecord;
+        let vm = this;
+        this.recognition.continuous = true;
+        this.recognition.interimResults = true;
+        this.recognition.start();
 
-      this.recognition.onresult = function(event) {
-        this.result_transcript = '';
-        for (let i = event.resultIndex; i < event.results.length; ++i) {
-          this.result_transcript += event.results[i][0].transcript;
-        }
-        document.getElementById('result').innerHTML = "Currently Saying: " + this.result_transcript;
-        for (let j = 0; j < vm.keyWord.length; j ++) {
-          if (this.result_transcript.indexOf(vm.keyWord[j]) > -1) {
-            vm.vibrateSimple();
-            vm.arreter();
+        this.recognition.onresult = function (event) {
+          this.result_transcript = '';
+          for (let i = event.resultIndex; i < event.results.length; ++i) {
+            this.result_transcript += event.results[i][0].transcript;
           }
-        }
-      };
+          document.getElementById('result').innerHTML = "Currently Saying: " + this.result_transcript;
+          for (let j = 0; j < vm.keyWord.length; j++) {
+            if (this.result_transcript.indexOf(vm.keyWord[j]) > -1) {
+              vm.vibrateSimple();
+              vm.arreter();
+            }
+          }
+        };
+      }
+      else{
+        this.isRecord = !this.isRecord;
+        this.recognition.stop();
+      }
     },
-    arreter() {
-      this.recognition.stop();
-    },
-
   }
 }
 </script>
